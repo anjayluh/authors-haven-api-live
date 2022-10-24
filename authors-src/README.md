722c29ea2d751a9c5e8698ec9180e47747f2a07e
angella naigaga <anjayluhgags.wakabi@gmail.com>
2022-10-24 14:18:07 +0300

    Intial commit

---
+++ authors-src/README.md
@@ -0,0 +1,2 @@
+# authors-src
+# authors-src
---
+++ authors-src/authors_api/settings/local.py
@@ -0,0 +1,5 @@
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-fren4ac853p&9_)a#4r*ttd!is7p$b_x2s*b%gcss#6$8ga5)n'
+
+ALLOWED_HOSTS = []
